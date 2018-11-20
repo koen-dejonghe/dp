9935efaa21f899098f42511535110be6f4ef3099
Koen Dejonghe <koen@fietsoverland.com>
2018-11-20 10:46:12 +0100

    std ci

--- src/main/scala/botkop/dp/mutable/DpApp.scala
+++ src/main/scala/botkop/dp/mutable/DpApp.scala
@@ -1,5 +1,6 @@
-package botkop.dp
+package botkop.dp.mutable
 
+import scala.annotation.tailrec
 import scala.language.postfixOps
 import scala.util.Random
 
@@ -14,12 +15,7 @@ case class Variable(var data: Double, f: Option[Function] = None) {
   def backward(): Unit = backward(1)
 
   def +(v: Variable): Variable = Add(this, v).forward()
-  def -(v: Variable): Variable = Min(this, v).forward()
   def *(v: Variable): Variable = Mul(this, v).forward()
-  def *(d: Double): Variable = MulConstant(this, d).forward()
-  def **(d: Double): Variable = PowConstant(this, d).forward()
-
-  def ~>(cf: Variable => Variable): Variable = cf(this)
 }
 
 trait Function {
@@ -35,36 +31,11 @@ case class Add(a: Variable, b: Variable) extends Function {
   }
 }
 
-case class Min(a: Variable, b: Variable) extends Function {
-  override def forward(): Variable = Variable(a.data - b.data, Some(this))
-  override def backward(g: Double): Unit = {
-    a.backward(g)
-    b.backward(-g)
-  }
-}
-
 case class Mul(a: Variable, b: Variable) extends Function {
   override def forward(): Variable = Variable(a.data * b.data, Some(this))
   override def backward(g: Double): Unit = {
     a.backward(b.data * g)
     b.backward(a.data * g)
-  }
-}
-
-case class MulConstant(v: Variable, d: Double) extends Function {
-  override def forward(): Variable = Variable(v.data * d, Some(this))
-  override def backward(gradOutput: Double): Unit = {
-    val dv = gradOutput * d
-    v.backward(dv)
-  }
-}
-
-case class PowConstant(v: Variable, d: Double) extends Function {
-  override def forward(): Variable = Variable(math.pow(v.data, d), Some(this))
-  val cache: Double = d * math.pow(v.data, d - 1)
-  override def backward(gradOutput: Double): Unit = {
-    val dv = cache * gradOutput
-    v.backward(dv)
   }
 }
 
@@ -88,39 +59,85 @@ abstract class Module {
   def apply(x: Variable): Variable = forward(x)
 }
 
+case class Net(m: Module, mlf: (Variable, Variable) => Function, o: Optimizer) {
+
+  def learnUntil(xys: Iterator[(Variable, Variable)],
+                 condition: (Int, Double) => Boolean): (Int, Double) = {
+    @tailrec
+    def lu(i: Int = 1): (Int, Double) = {
+      val l = learn(xys)
+      println(l)
+      if (!condition(i, l))
+        lu(i + 1)
+      else
+        (i, l)
+    }
+    lu()
+  }
+
+  def learn(xys: Iterator[(Variable, Variable)]): Double = {
+    val (l, n) = xys.foldLeft(0.0, 0) {
+      case ((zl, zi), (x, y)) =>
+        (zl + learn(x, y), zi + 1)
+    }
+    l / n
+  }
+
+  def learn(x: Variable, y: Variable): Double = {
+    o.zeroGrad()
+    val yHat = m(x)
+    val l = mlf(yHat, y).forward()
+    l.backward()
+    o.step()
+    l.data
+  }
+
+  def apply(x: Variable): Variable = m(x)
+}
+
 object DpApp extends App {
 
-  def function(x: Double): Double = 2 * x + 37
+  def function(x: Double): Double = x / 2 + 3
   val numSamples = 10
 
-  val (xs, ys) = (1 to numSamples) map { i =>
-    val x = i.toDouble
+  val xys = (1 to numSamples) map { _ =>
+    val x = Random.nextDouble()
     val y = function(x)
     (Variable(x), Variable(y))
-  } unzip
+  }
 
-  case class Loss(actual: Variable, target: Variable) extends Function {
+  case class SimpleLoss(actual: Variable, target: Variable) extends Function {
     val diff: Double = actual.data - target.data
     override def forward(): Variable =
       Variable(diff, Some(this))
-    override def backward(g: Double /* ignored */ ): Unit =
+    override def backward(g: Double): Unit =
       actual.backward(diff)
   }
 
+  def makeLossFunction(x: Variable, y: Variable): Function = SimpleLoss(x, y)
+
   val m: Module = new Module() {
-    val ws: Seq[Variable] = Seq.fill(2) { Variable(Random.nextDouble()) }
-    val bs: Seq[Variable] = Seq.fill(2) { Variable(0) }
-    override def parameters: Seq[Variable] = ws ++ bs
-    override def forward(x: Variable): Variable = ws.zip(bs).foldLeft(x) {
-      case (z, (w, b)) =>
-        z + w + b
-    }
+    val w: Variable = Variable(Random.nextDouble())
+    val b: Variable = Variable(0)
+    override def parameters: Seq[Variable] = Seq(w, b)
+    override def forward(x: Variable): Variable =
+      x * w + b
   }
 
   val o = SGD(m.parameters, 1e-4)
 
+  val n = Net(m, makeLossFunction, o)
+
+  n.learnUntil(xys.iterator, (i, _) => i > 1000)
+
+  val x = 11
+  val yh = n(Variable(x)).data
+  val y = function(x)
+  println(s"x=$x f(x)=$y y^=$yh")
+
+  /*
   val printEvery = 100
-  for (it <- 1 to 2000) {
+  for (it <- 1 to 10000) {
     val loss = xs.zip(ys).foldLeft(0.0) {
       case (z, (x, y)) =>
         m.zeroGrad()
@@ -139,6 +156,6 @@ object DpApp extends App {
       println(s"$it x=$x f(x)=$y y^=$yh ($l)")
     }
   }
-
+ */
 
 }
